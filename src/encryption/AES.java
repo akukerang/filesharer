e184aa45c3c3dab4790c0f237c0e252f5e1f491a
aku <gabrielsuoth@gmail.com>
2023-03-25 12:17:48 -0400

    mixcolumn, keyschedule

--- src/encryption/AES.java
+++ src/encryption/AES.java
@@ -1,7 +1,7 @@
 package encryption;
 import math.BigInt;
 public class AES {
-    String[][] sBox = {
+    static String[][] sBox = {
         {"63", 	"7c", 	"77", 	"7b", 	"f2", 	"6b", 	"6f", 	"c5", 	"30", 	"01", 	"67", 	"2b", 	"fe", 	"d7", 	"ab", 	"76"},
         {"ca", 	"82", 	"c9", 	"7d", 	"fa", 	"59", 	"47", 	"f0", 	"ad", 	"d4", 	"a2", 	"af", 	"9c", 	"a4", 	"72", 	"c0"},
      	{"b7", 	"fd", 	"93", 	"26", 	"36", 	"3f", 	"f7", 	"cc", 	"34", 	"a5", 	"e5", 	"f1", 	"71", 	"d8", 	"31", 	"15"},
@@ -18,6 +18,14 @@ public class AES {
      	{"70", 	"3e", 	"b5", 	"66", 	"48", 	"03", 	"f6", 	"0e", 	"61", 	"35", 	"57", 	"b9", 	"86", 	"c1", 	"1d", 	"9e"},
      	{"e1", 	"f8", 	"98", 	"11", 	"69", 	"d9", 	"8e", 	"94", 	"9b", 	"1e", 	"87", 	"e9", 	"ce", 	"55", 	"28", 	"df"},
      	{"8c", 	"a1", 	"89", 	"0d", 	"bf", 	"e6", 	"42", 	"68", 	"41", 	"99", 	"2d", 	"0f", 	"b0", 	"54", 	"bb", 	"16"}, 
+    };
+
+
+    static int[][] mixPublic = {
+        {2,3,1,1},
+        {1,2,3,1},
+        {1,1,2,3},
+        {3,1,1,2}
     };
 
     BigInt key;
@@ -76,11 +84,84 @@ public class AES {
         return shift;
     }
 
-    public static String[][] mixColumn(String[][] block){
+
+    public static String[][] mixColumn(String[][] block) {
         String[][] mixed = new String[4][4];
-
-
+        int[] temp = new int[4];
+        for (int i = 0; i < 4; i++) { // Column
+            for (int j = 0; j < 4; j++) { // Row
+                for (int k = 0; k < 4; k++) {
+                    int element = Integer.parseInt(block[k][i], 16);
+                    int mix = mixPublic[j][k];
+                    temp[k] = multiply(element, mix);
+                }
+                mixed[j][i] = Integer.toHexString((temp[0] ^ temp[1] ^ temp[2] ^ temp[3]) & 0xff); //overflow
+            }
+        }
         return mixed;
+    }
+    
+    private static int multiply(int a, int b) {
+        int p = 0;
+        for (int i = 0; i < 8; i++) {
+            if ((b & 1) != 0) {
+                p ^= a;
+            }
+            boolean carry = (a & 0x80) != 0;
+            a <<= 1;
+            if (carry) {
+                a ^= 0x1b;
+            }
+            b >>= 1;
+        }
+        return p;
+    }
+    
+    public static String[][] addRoundKey(String[][] block, String[][] key){
+        String[][] added = new String[4][4];
+
+        return added;
+    }
+
+    public static String[] keySchedule(String key){ // Input binary, Output binary
+        String[] keys = new String[11];
+        StringBuilder str = new StringBuilder();
+        keys[0] = "0".repeat(128 - key.length()) + key;
+        String temp;
+        for(int i = 1; i < 11; i++){
+            temp = xor(keys[i-1].substring(0,32), //w[0] xor w[3]
+                keys[i-1].substring(96,128));
+            str.append(temp);
+            temp = xor(temp, keys[i-1].substring(32,64)); // temp xor w[1]
+            str.append(temp);
+            temp = xor(temp, keys[i-1].substring(64,96)); // temp xor w[2]
+            str.append(temp);
+            temp = xor(temp, keys[i-1].substring(96,128)); // temp xor w[3]
+            str.append(temp);
+            keys[i] = str.toString();
+            str = new StringBuilder();
+        }
+        return keys;
+    }
+
+    public static String xor(String a, String b){
+        //padded to 32 bit
+        if(a.length() < 32){
+            a = "0".repeat(32 - a.length()) + a;
+        }
+        if(b.length() < 32){
+            b = "0".repeat(32 - b.length()) + b;
+        }
+        StringBuilder str = new StringBuilder();
+        for(int i = 0; i < 32; i++){
+            if(a.charAt(i) == b.charAt(i)){
+                str.append("0");
+            } else {
+                str.append("1");
+            }
+        }
+        return str.toString();
+
     }
 
 
@@ -110,18 +191,25 @@ public class AES {
     }
 
     public static void main(String[] args) {
-        byte[] temp = {-128,32,12,12,12,-23,-1,0,-128,32,12,12,12,-23,-1,0};
-        for(int i = 0; i < temp.length; i++){
-            System.out.println(Byte.toUnsignedInt(temp[i]));
+
+
+        String[][] temp = {
+            {"63","eb","9f","a0"},
+            {"2f","93","92","c0"},
+            {"af","c7","ab","30"},
+            {"a2","20","cb","2b"},
+
+        };
+
+        String[] keys = keySchedule("10101010100100010001011100");
+
+        for(int i = 0; i < keys.length; i++){
+            System.out.println(keys[i]);
         }
-        String[][] temp2 = bytesToHex(temp);
-        for(int i = 0; i < 4; i++){
-            for(int j = 0; j < 4; j++){
-                System.out.print(temp2[i][j]+ " ");
-            }
-            System.out.println();
-        }
+    
     }
 
 }
+
+
 
